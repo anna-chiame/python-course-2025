da6ff5c84795c2834fab77630ee70706c644b04a
anna-chiame <annatolmats@gmail.com>
2025-10-07 12:12:35 +0300

    Add files via upload
    Task 3
    List comprehension exercise
    Use a list comprehension to make a list containing tuples (i, j) where 'i' goes from 1 to 10 and 'j' is corresponding to 'i' squared.

---
+++ HW7_3.py
@@ -0,0 +1,18 @@
+# ask user to enter a sentence
+sentence = input("Enter a sentence: \n")
+
+# split the sentence into words
+words = sentence.split()
+
+#  empty dictionary
+word_count = {}
+
+# count occurrences of each word
+for word in words:
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
+
+
+print(word_count)
