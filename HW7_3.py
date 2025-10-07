cd4bb062dfef80287ca382444e8f7364e7bb10cb
anna-chiame <annatolmats@gmail.com>
2025-10-07 12:16:58 +0300

    Update HW7_3.py

--- HW7_3.py
+++ HW7_3.py
@@ -1,18 +1,5 @@
-# ask user to enter a sentence
-sentence = input("Enter a sentence: \n")
+result = [(i, i**2) for i in range(1, 11)]
 
-# split the sentence into words
-words = sentence.split()
-
-#  empty dictionary
-word_count = {}
-
-# count occurrences of each word
-for word in words:
-    if word in word_count:
-        word_count[word] += 1
-    else:
-        word_count[word] = 1
-
-
-print(word_count)
+# list comprehension to generate list of tuples
+result = [(i, i**2) for i in range(1, 11)]
+print(result)
